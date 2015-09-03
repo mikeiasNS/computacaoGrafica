ddfad98776d6c322ee77ff1eeb8e21560d045db7
Mikeias <mikeias66@gmail.com>
2015-09-03 00:23:10 -0300

    scanLine rules

--- js/scanLine.js
+++ js/scanLine.js
@@ -1,6 +1,8 @@
 function scanLine(vertices, cor){
+	console.log(vertices);
+
 	var maiorY = vertices[0][1];
-	var menorY = vertices[0][0];
+	var menorY = vertices[0][1];
 	for(var i = 1; i < vertices.length; i++){
 		if(vertices[i][1] > maiorY){
 			maiorY = vertices[i][1];
@@ -18,38 +20,28 @@ function scanLine(vertices, cor){
 			var y0a, yfa, ta, x0a, xfa;
 			if(j == vertices.length-1){
 				// compare com o primeiro
-				// y0 e yf
 				if(vertices[j][1] > vertices[0][1]){
 					y0a = vertices[0][1];
 					yfa = vertices[j][1];
-				} else{
-					yfa = vertices[0][1];
-					y0a = vertices[j][1];
-				}
-				// x0 e xf
-				if(vertices[j][0] > vertices[0][0]){
 					x0a = vertices[0][0];
 					xfa = vertices[j][0];
 				} else{
+					yfa = vertices[0][1];
+					y0a = vertices[j][1];
 					xfa = vertices[0][0];
 					x0a = vertices[j][0];
 				}
 			}else{
 				// compare com o proximo
-				// y0 e yf
 				if(vertices[j][1] > vertices[j + 1][1]){
 					y0a = vertices[j + 1][1];
 					yfa = vertices[j][1];
+					x0a = vertices[j + 1][0];
+					xfa = vertices[j][0];
 				} else{
 					yfa = vertices[j + 1][1];
 					y0a = vertices[j][1];
-				}
-				// x0 e xf
-				if(vertices[j][0] > vertices[0][0]){
-					x0a = vertices[j+1][0];
-					xfa = vertices[j][0];
-				} else{
-					xfa = vertices[j+1][0];
+					xfa = vertices[j + 1][0];
 					x0a = vertices[j][0];
 				}
 			}
@@ -62,8 +54,10 @@ function scanLine(vertices, cor){
 				intersecoes.push(xDaIntersecao);
 			}
 		}
+		intersecoes = intersecoes.sort();
 		// pinta a linha
-		for (var k = 0; k < intersecoes.length - 1; k++) {
+		for (var k = 0; k < intersecoes.length - 1; k+=2) {
+			var teste = intersecoes[k];
 			bresenhan( [intersecoes[k], i], [intersecoes[k + 1], i], cor );
 		}
 	}
